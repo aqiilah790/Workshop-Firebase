234ebb2da9abbcdbfd042cd4909481f5502c3fbc
muhammad aqiilah paligie <127171345+aqiilah790@users.noreply.github.com>
2025-10-23 15:29:32 +0700

    penambahan baru

--- firebase_workshop/lib/main.dart
+++ firebase_workshop/lib/main.dart
@@ -2,11 +2,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyDQ9pTzxv7ztEI2lSZbbhTCviRiACZ4nOE",
+        authDomain: "fir-workshop-b6997.firebaseapp.com",
+        projectId: "fir-workshop-b6997",
+        storageBucket: "fir-workshop-b6997.firebasestorage.app",
+        messagingSenderId: "638779278399",
+        appId: "1:638779278399:web:d2d77815f9b067c485a48a",
+        measurementId: "G-2RVX119GH5"),
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
